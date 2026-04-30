4b3dfe23c7bc6f854522b1e9be55b3c652633870
Mohammed Ali <hussainalimohammed39@gmail.com>
2026-04-30 23:43:39 +0300

    Add Upload File Service Like img and docx

---
+++ Services/IUploadFils.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MyShope.Services
+{
+    public interface IUploadFils
+    {
+        string? UploadFile(IFormFile upload, string subFolder = "ProuctsImg");
+    }
+}
---
+++ Services/UploadDocxFile.cs
@@ -0,0 +1,62 @@
+﻿using MyShope.Services;
+
+namespace Services.Services
+{
+    public class UploadDocxFile:IUploadFils
+    {
+
+       private readonly IWebHostEnvironment _environment;
+
+        private string[] allowExtend = { ".pdf", ".doc", ".docx" ,".rar",".zip"};
+        public int fileSize = 4 * 1024 * 1024;
+
+
+        public UploadDocxFile(IWebHostEnvironment environment)
+        {
+            this._environment = environment;
+        }
+
+        
+
+        public string basePath = "Docx";
+        
+
+        public String UploadFile(IFormFile upload, string subFolder = "DocxFile")
+        {
+            string fileUrl = string.Empty;
+
+            if (upload.Length > fileSize)
+            {
+                throw new Exception($"Allowed File Size is {fileSize / 1024 / 1024} Mg");
+            }
+
+            if (!allowExtend.Contains(Path.GetExtension(upload.FileName)))
+            {
+                throw new Exception($"Allowed File type is {String.Join(",", allowExtend)} ");
+
+            }
+
+            
+
+            var FileName = Guid.NewGuid().ToString() + Path.GetExtension(upload.FileName);
+
+            var FilePath = Path.Combine(_environment.WebRootPath, basePath, subFolder, FileName);
+
+            using (var fileStream = System.IO.File.Create(FilePath))
+            {
+                upload.CopyTo(fileStream);
+            }
+
+
+            fileUrl = $"/{basePath}/{subFolder}/{FileName}";
+
+            return fileUrl;
+
+
+        }
+
+
+
+
+    }
+}
