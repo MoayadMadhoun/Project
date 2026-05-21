9de0e485b1c7fcfc137f8aeb556508e163d247b3
AlaaAlzammar <alaaelzammar421203754@gmail.com>
2026-05-21 16:34:58 +0300

    Alaa rest of Opporuntities pages

---
+++ Project/Pages/University/AvailabelOpportunities.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages.University
+{
+    public class AvailabelOpportunitiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
