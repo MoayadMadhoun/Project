467fd1ff304ba82cb58970573bc69dc4fd4fd3b6
AlaaAlzammar <alaaelzammar421203754@gmail.com>
2026-06-01 08:41:41 +0300

    Alaa Add DepartmentsHeads Page

---
+++ Project/Pages/University/DepartmentsHeads.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages.University
+{
+    public class DepartmentsHeadsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
