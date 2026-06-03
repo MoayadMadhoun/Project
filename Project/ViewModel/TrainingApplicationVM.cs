98602d14f2f582cdd54d45ad1b0d26be908357d2
Mohammed Ali <hussainalimohammed39@gmail.com>
2026-06-03 18:01:32 +0300

    add ViewModel

---
+++ Project/ViewModel/TrainingApplicationVM.cs
@@ -0,0 +1,20 @@
+﻿using Project.Models;
+
+namespace Project.ViewModel
+{
+    public class TrainingApplicationVM
+    {
+        public int ApplicationID { get; set; }
+
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+
+        public string InstitutionName { get; set; }
+
+        public DateTime AppliedAt { get; set; }
+
+        public TrainingApplication.ApplicationStatus Status { get; set; }
+
+        public TrainingApplication.Decision DepartmentDecision { get; set; }
+    }
+}
