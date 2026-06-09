7859880e8ccd603e5b208577fba07c6afff2d3fb
Moayad Al-Madhoun <moayadalmadhoun@gmail.com>
2026-06-09 21:32:45 +0300

    upload add supervisors & deptHeads pages with CRUD

---
+++ Project/Pages/University/Supervisors/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project.Pages.University.Supervisors
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
