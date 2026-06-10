29eeb81e83123cc0e1ab274c175a52aab59378d0
Mohammed Ali <hussainalimohammed39@gmail.com>
2026-06-10 17:56:31 +0300

    Add Delete Page To Department Head

--- Project/Pages/University/DepartmentHeads/Delete.cshtml.cs
+++ Project/Pages/University/DepartmentHeads/Delete.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Models;
+using Project.Repostory;
+using System.Threading.Tasks;
 
 namespace Project.Pages.University.DepartmentHeads
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UniversityRepository _repUniversity;
+
+        public DeleteModel(ApplicationDbContext dbContext,UniversityRepository repUniversity)
         {
+           _dbContext = dbContext;
+            
+            _repUniversity = repUniversity;
+        }
+        public AspNetUser? DepartmentHead { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            DepartmentHead = await  _repUniversity.GetDpartmentHeadByUserId(id);
+            if (DepartmentHead is null)
+            {
+                ModelState.AddModelError(nameof(DepartmentHead), "·« ÌÊÃœ —∆Ì” ﬁ”„ ");
+                return Page();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost(string id)
+        {
+            var departmentHead = await _dbContext.Users .FirstOrDefaultAsync(u => u.Id == id);
+
+            if(departmentHead is null)
+            {
+                return NotFound();
+            }
+            departmentHead.IsActive = false;
+
+            await _dbContext.SaveChangesAsync();
+            TempData["SuccessMessafe"] = " „  ⁄ÿÌ· —∆Ì” «·ﬁ”„ »‰Ã«Õ";
+            return Page();
         }
     }
 }
