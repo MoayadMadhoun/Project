e727c8da610eb58df8e8462dcf0594f1e657e642
Mohammed Ali <hussainalimohammed39@gmail.com>
2026-06-10 20:41:34 +0300

    Add SuperVisor Page (Details  Page ){Back-End }

--- Project/Pages/University/Supervisors/Details.cshtml.cs
+++ Project/Pages/University/Supervisors/Details.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project.Data;
+using Project.Models;
+using Project.Repostory;
 
 namespace Project.Pages.University.Supervisors
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UniversityRepository _repUniversity;
+
+        public DetailsModel(ApplicationDbContext dbContext, UniversityRepository repUniversity)
         {
+            _dbContext = dbContext;
+            _repUniversity = repUniversity;
+        }
+
+        public AspNetUser? Supervisor { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+            Supervisor = await _repUniversity.GetUniversitySuperVisorByUserId(id);
+
+            if (Supervisor == null)
+            {
+                ModelState.AddModelError(nameof(Supervisor), "·« ÌÊÃœ —∆Ì” Ã«„⁄… ");
+
+                return Page();
+            }
+            return Page();
         }
     }
 }
