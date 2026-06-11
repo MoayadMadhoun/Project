606a5f24944fbff3a202a60e93d8430b14f4cc4f
Mohammed Ali <hussainalimohammed39@gmail.com>
2026-06-11 19:27:36 +0300

    Update in SuperVisorVM Of University

--- Project/Pages/University/ViewModels/SupervisorsVM.cs
+++ Project/Pages/University/ViewModels/SupervisorsVM.cs
@@ -25,7 +25,7 @@ namespace Project.Pages.University.ViewModels
         [StringLength(10, MinimumLength = 8, ErrorMessage = "الرقم يجب ان يكون من عشر خانات كحد ادنى ")]
         [RegularExpression(@"^[0-9]+$", ErrorMessage = "يجب ان يحتوي على ارقام فقط )")]
         public string? PhoneNumber { get; set; }
-
+        public string? UniversityName { get; set; }
         public bool IsActive { get; set; }
     }
 }
