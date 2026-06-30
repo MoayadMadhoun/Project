a349b2d2f97d00c0b8a3642ce2bc11b46db2aa46
Moayad Al-Madhoun <moayadalmadhoun@gmail.com>
2026-06-30 16:00:50 +0300

    increase UI UX efficency

--- Project/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Project/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -36,8 +36,12 @@ namespace Project.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [Display(Name = "البريد الالكتروني الحالي")]
         public string Email { get; set; }
 
+
+        [TempData]
+        public string ErrorMessage { get; set; }
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -70,7 +74,7 @@ namespace Project.Areas.Identity.Pages.Account.Manage
             /// </summary>
             [Required]
             [EmailAddress]
-            [Display(Name = "New email")]
+            [Display(Name = "البريد الالكتروني الجديد")]
             public string NewEmail { get; set; }
         }
 
